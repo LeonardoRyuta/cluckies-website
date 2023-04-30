3d2b93f1b6124acc4e332df14ae2a739c28f0e2f
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2023-04-30 19:10:32 +0100

    Update [id].ts

--- cluckies/pages/api/tokens/[id].ts
+++ cluckies/pages/api/tokens/[id].ts
@@ -3,7 +3,7 @@ import connectDb from '../../../lib/mongodb'
 
 export default async function handler(req : NextApiRequest, res : NextApiResponse) {
     const db = await connectDb();
-    const { id } = req.query;
+    const { id }:any = req.query;
 
     const tokenId = await db.collection("Metadata").find({ tokenId: parseInt(id) }).toArray();
     const tokenData = tokenId[0];
